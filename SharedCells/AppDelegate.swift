d913c48e8454b9331ce6f149b32e5c89e49017ba
Thomas Vachuska <tom@onlab.us>
2016-05-13 07:15:56 -0400

    Initial Commit

---
+++ SharedCells/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SharedCells
+//
+//  Created by Thomas Vachuska on 5/13/16.
+//  Copyright © 2016 Thomas Vachuska. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
