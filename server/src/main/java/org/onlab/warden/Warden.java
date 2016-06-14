2245a9308ce08213063d1aa1a3f642caa2a0155c
Thomas Vachuska <tom@onlab.us>
2016-06-14 11:15:06 -0700

    Added protection against unreasonable cell specs.

--- server/src/main/java/org/onlab/warden/Warden.java
+++ server/src/main/java/org/onlab/warden/Warden.java
@@ -33,13 +33,11 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.*;
 
@@ -178,7 +176,7 @@ class Warden {
         checkNotNull(sshKey, KEY_NOT_NULL);
         checkArgument(minutes < MAX_MINUTES, "Number of minutes must be less than %d", MAX_MINUTES);
         checkArgument(minutes >= 0, "Number of minutes must be non-negative");
-        checkArgument(cellSpec == null || cellSpec.matches("[\\d]+\\+[0-1]"),
+        checkArgument(cellSpec == null || cellSpec.matches("[\\d]\\+[0-1]"),
                       "Invalid cell spec string %s", cellSpec);
         Reservation reservation = currentUserReservation(userName);
         if (reservation == null) {
