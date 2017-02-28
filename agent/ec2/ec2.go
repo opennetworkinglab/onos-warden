8bf7150964ea797f415691660f16f45124ca60e8
Brian O'Connor <bocon@onlab.us>
2017-02-27 17:38:33 -0800

    reverting AgentAds

--- agent/ec2/ec2.go
+++ agent/ec2/ec2.go
@@ -7,7 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/opennetworkinglab/onos-warden/agent"
 	"github.com/opennetworkinglab/onos-warden/warden"
-	"time"
+	"strconv"
 )
 
 const DefaultAwsRegion string = "us-west-1"
@@ -38,11 +38,18 @@ func (c *ec2Client) Bind(client agent.WardenClient) {
 
 func (c *ec2Client) Start() {
 	go func() {
-		for {
-			c.updateInstances()
-			c.client.PublishUpdate(&warden.ClusterAdvertisement{})
-			time.Sleep(5 * time.Second)
+		for i, _ := range slots {
+			c.client.PublishUpdate(&warden.ClusterAdvertisement{
+				ClusterId: strconv.Itoa(i),
+				ClusterType: "ec2",
+			})
 		}
+		c.updateInstances()
+		//for {
+		//	c.updateInstances()
+		//	c.client.PublishUpdate(&warden.ClusterAdvertisement{})
+		//	time.Sleep(5 * time.Second)
+		//}
 	}()
 }
 
@@ -54,9 +61,20 @@ func (c *ec2Client) Handle(req *warden.ClusterRequest) {
 	//TODO
 }
 
+func getReservation(i *ec2.Instance) (warden.ClusterAdvertisement, error) {
+	return nil, nil
+}
+
 func (c *ec2Client) updateInstances() {
 	// Call the DescribeInstances Operation
-	resp, err := c.svc.DescribeInstances(nil)
+	fmt.Println("starting...")
+
+	filter := ec2.Filter{
+		Name: aws.String("tag-key"),
+		Values: aws.StringSlice([]string{"Cell-Id"}),
+	}
+	in := ec2.DescribeInstancesInput{Filters: []*ec2.Filter{&filter}}
+	resp, err := c.svc.DescribeInstances(&in)
 	if err != nil {
 		panic(err)
 	}
@@ -67,10 +85,41 @@ func (c *ec2Client) updateInstances() {
 		fmt.Println("  > Number of instances: ", len(res.Instances))
 		for _, inst := range resp.Reservations[idx].Instances {
 			fmt.Println("    - Instance ID: ", *inst.InstanceId)
+			fmt.Println(inst.Tags)
 		}
 	}
+//-------------------------------------
+//	params := &ec2.DescribeInstancesInput{
+//		Filters: []*ec2.Filter{
+//			//{
+//			//	Name:   aws.String("tag:Environment"),
+//			//	Values: []*string{aws.String("prod")},
+//			//},
+//			{
+//				Name:   aws.String("instance-state-name"),
+//				Values: []*string{aws.String("running")},
+//			},
+//		},
+//	}
+//	resp, err := c.svc.DescribeInstances(params)
+//	if err != nil {
+//		fmt.Println("there was an error listing instances in", err.Error())
+//		log.Fatal(err.Error())
+//	}
+//
+//	for idx, res := range resp.Reservations {
+//		fmt.Println("  > Reservation Id", *res.ReservationId, " Num Instances: ", len(res.Instances))
+//		for _, inst := range resp.Reservations[idx].Instances {
+//			fmt.Println("    - Instance ID: ", *inst.InstanceId)
+//		}
+//	}
 }
 
+// name of cell
+// reservation info
+
+var slots []struct{}
 func main() {
+	slots = make([]struct{}, 10)
 	agent.Run(NewEC2Client(DefaultAwsRegion))
 }
