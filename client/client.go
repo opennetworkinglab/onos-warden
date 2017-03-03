0a10dfda827dcc99483e1cb3c9632984d66a6849
Brian O'Connor <bocon@onlab.us>
2017-03-02 19:21:29 -0800

    Making the client smarter
    It requests one cell and waits until the cell is provisioned

--- client/client.go
+++ client/client.go
@@ -7,9 +7,25 @@ import (
 	"google.golang.org/grpc/grpclog"
 	"io"
 	"fmt"
+	"os"
+	"os/user"
+	"flag"
+	"math/rand"
+	"time"
 )
 
+// Request the first available cell, return when cell reservation is completed
 func main() {
+	rand.Seed(time.Now().Unix())
+	currUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	username := flag.String("user", currUser.Username, "username for reservation; defaults to $USER")
+	key := flag.String("key", "", "public key for SSH")
+	duration := flag.Int64("duration", -1, "duration of reservation in minutes; -1 is unlimited")
+	nodes := flag.Uint64("nodes", 3, "number of nodes in cell; defaults to 3")
+
 	conn, err := grpc.Dial("127.0.0.1:1234", grpc.WithInsecure())
 	if err != nil {
 		grpclog.Fatalf("fail to dial: %v", err)
@@ -21,42 +37,49 @@ func main() {
 	if err != nil {
 		grpclog.Fatalf("%v.ServerClusters(_) = _, %v", client, err)
 	}
+	defer stream.CloseSend()
 
-	count := 0
-	waitc := make(chan struct{})
-	go func() {
-		for {
-			in, err := stream.Recv()
-			if err == io.EOF {
-				// read done.
-				close(waitc)
-				return
-			}
-			if err != nil {
-				grpclog.Fatalf("Failed to receive: %v", err)
-			}
-			grpclog.Println("Got message:", in)
-			if in.State == warden.ClusterAdvertisement_AVAILABLE {
-				count++
+	var cluster *warden.ClusterAdvertisement
+	var reqId string
+	for {
+		ad, err := stream.Recv()
+		if err == io.EOF {
+			// stream closed
+			grpclog.Fatalln("Stream closed unexpectedly")
+			os.Exit(1)
+		}
+		if err != nil {
+			grpclog.Fatalf("Failed to receive: %v", err)
+		}
+		grpclog.Println("Got message:", ad)
+		if cluster == nil {
+			// pick the first available cluster
+			if ad.State == warden.ClusterAdvertisement_AVAILABLE {
+				cluster = ad
+				reqId = fmt.Sprintf("res-%x", rand.Int())
 				stream.Send(&warden.ClusterRequest{
-					RequestId: fmt.Sprintf("res-%d", count),
-					Type: warden.ClusterRequest_RESERVE,
-					ClusterId: in.ClusterId,
-					ClusterType: in.ClusterType,
-					Duration: 0,
+					RequestId:   reqId,
+					Type:        warden.ClusterRequest_RESERVE,
+					ClusterId:   ad.ClusterId,
+					ClusterType: ad.ClusterType,
+					Duration:    int32(*duration),
 					Spec: &warden.ClusterRequest_Spec{
-						ControllerNodes: uint32(3),
-						UserName: "client",
-						UserKey: "myawesomepubkey-rsa",
+						ControllerNodes: uint32(*nodes),
+						UserName: *username,
+						UserKey: *key,
 					},
 				})
-				stream.CloseSend()
-				close(waitc)
+			}
+
+		} else if cluster.ClusterId == ad.ClusterId && cluster.ClusterType == cluster.ClusterType {
+			if ad.RequestId != reqId {
+				// cluster was assigned to someone else; try again...
+				cluster = nil
+				reqId = ""
+			} else if ad.State == warden.ClusterAdvertisement_RESERVED {
+				fmt.Println("Got cluster:", ad);
 				break
 			}
 		}
-	}()
-
-	//stream.CloseSend()
-	<-waitc
+	}
 }
